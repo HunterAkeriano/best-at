c6c0a73c2d54f270b239f1556421d3e8d6607c5f
HunterAkeriano <stajectgamer@gmail.com>
2023-07-24 15:22:35 +0300

    dima-dev: откат от страницы

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,10 @@
 <template>
    <h2>Главная страница</h2>
-   <TheHeader
-   :width="262"
-   :padding="20"
-   >Начните сейчас</TheHeader>
 </template>
 
 
 <script setup>
-import TheHeader from '@/components/UI/Buttons/Button.vue'
+
 </script>
 
 
