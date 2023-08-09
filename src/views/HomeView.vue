fdbcecae7c98975710707aba8fc596db04fcd414
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2023-08-09 22:18:03 +0300

    added components on the page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,14 @@
     <HowWeWork />
 
     <OurAdvantages />
+
+    <div class="teachers" />
+
+    <div class="reviews" />
+
+    <WeAreTrusted />
+
+    <TrialLesson />
   </div>
 </template>
 
@@ -16,10 +24,18 @@ import TeachersSwiper from '@/components/Base/TeachersSwiper.vue'
 import PopularCourses from '@/components/Base/PopularCourses.vue'
 import HowWeWork from '@/components/Base/HowWeWork.vue'
 import OurAdvantages from '@/components/Base/OurAdvantages.vue'
+import WeAreTrusted from '@/components/Base/WeAreTrusted.vue'
+import TrialLesson from '@/components/Base/TrialLesson.vue'
 </script>
 
 
 <style lang="scss">
+.teachers {
+  height: 854px;
+}
 
+.reviews {
+  height: 571px;
+}
 </style>
 
