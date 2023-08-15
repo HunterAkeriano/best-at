0f30ae416d06d6b8910261507e2b996afc46e5b0
HunterAkeriano <stajectgamer@gmail.com>
2023-08-15 11:24:58 +0300

    dima-dev: Добавление селекта

---
+++ src/components/UI/TheSelect/TheSelect.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="select-comp" :class="{'select-comp_opened': stateMenu}">
+    <div class="select-comp__selected" @click="openMenu">
+      <p>{{ SELECTED_ELEMENT.title }}</p>
+      <IconArrow/>
+    </div>
+    <div class="select-comp__item">
+      <p v-for="(item, idx) in items" @click="selectedItem(idx)">{{ item.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+
+import IconArrow from '@/assets/icons/header/Arrow.vue'
+
+
+const SELECTED_ELEMENT = ref(props.items[0])
+
+const stateMenu = ref(false)
+function openMenu(){
+  stateMenu.value = !stateMenu.value;
+}
+
+const emits = defineEmits(['setActiveSelect', 'click',]);
+function selectedItem(idx){
+  stateMenu.value = false;
+  SELECTED_ELEMENT.value = props.items[idx];
+  emits('setActiveSelect', idx);
+}
+
+
+const props = defineProps({
+  items: {
+    type: Array,
+    default: [],
+  },
+})
+</script>
+
+<style lang="scss">
+.select-comp{
+  font-family: Montserrat;
+  position: relative;
+  cursor: pointer;
+
+  &__selected{
+    border-radius: 20px;
+    background: #F2F5FA;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-right: 13px;
+
+    p{
+      color: #454B58;
+      font-weight: 500;
+      line-height: 24px;
+      font-size: 13px;
+      padding-left: 20px;
+      padding-top: 5px;
+      padding-bottom: 6px;
+    }
+
+    svg{
+      transition: all .25s;
+    }
+  }
+
+  &__item{
+    overflow: hidden;
+    position: absolute;
+    max-height: 0;
+    opacity: 0;
+    left: 0;
+    top: 33px;
+    width: 100%;
+    border-radius: 0 0 20px 20px;
+    background: #F2F5FA;
+    transition: all .25s;
+
+    p{
+      color: #454B58;
+      font-weight: 500;
+      line-height: 24px;
+      font-size: 13px;
+      padding-left: 20px;
+    }
+  }
+
+  &_opened{
+    svg{
+      transform: rotate(180deg);
+    }
+    .select-comp__selected{
+      border-radius: 0;
+      border-radius: 20px 20px 0 0;
+    }
+    .select-comp__item{
+      max-height: 200px;
+      opacity: 1;
+      overflow: auto;
+      z-index: 10;
+      padding-bottom: 20px;
+
+      p:hover{
+        background-color: rgb(242, 241, 241);
+      }
+    }
+
+    .select-comp__item::-webkit-scrollbar{
+      width: 5px;
+    }
+
+    .select-comp__item::-webkit-scrollbar-track{
+      background: rgb(164, 163, 163);
+    }
+
+    .select-comp__item::-webkit-scrollbar-thumb{
+      background: rgb(99, 98, 98);
+    }
+  }
+}
+</style>
