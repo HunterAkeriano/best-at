88cd98809abf4fd1f58c81e049a2af443dd57187
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2023-09-06 11:46:47 +0300

    added article page

--- src/router/index.js
+++ src/router/index.js
@@ -65,6 +65,11 @@ const router = createRouter({
       component: () => import("@/views/forum/ForumTheme.vue" /* webpackChunkName: "registration" */),
     },
     {
+      name: 'Article Page',
+      path: '/article',
+      component: () => import("@/views/ArticleView.vue" /* webpackChunkName: "article" */),
+    },
+    {
       name: 'Course Create',
       path: '/course-create',
       component: () => import("@/views/Course/CourseCreate.vue" /* webpackChunkName: "registration" */),
